82f38aa01ad998c8d0a2ef4af9233dc1f62c315d
SeokYong <132437309+Yoon0917@users.noreply.github.com>
2024-06-02 17:35:10 +0900

    st

---
+++ src/main/java/com/ohgiraffers/policykorea/PolicyKoreaApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.policykorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyKoreaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolicyKoreaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ohgiraffers/policykorea/login/Login.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.policykorea.login;
+
+public class Login {
+}
