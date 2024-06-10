1b82d623d30f8498eec0954c3bc04fdc96c221fa
SeokYong <132437309+Yoon0917@users.noreply.github.com>
2024-06-10 09:28:39 +0900

    add html

---
+++ src/main/java/com/ohgiraffers/policykorea/board/BoardController.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.policykorea.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board/*")
+public class BoardController {
+
+
+    @RequestMapping("/view")
+    public String boardmain(){
+
+        return "board";
+    }
+}
