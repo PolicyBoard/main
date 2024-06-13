1cb3d0d23fd8ca9dac7eef6db071f0275cab6cd2
nayeon <nayeon08911@gmail.com>
2024-06-14 02:52:03 +0900

    jkk

--- src/main/java/com/ohgiraffers/policykorea/signup/config/SpringConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.ohgiraffers.policykorea.signup.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.ohgiraffers.policykorea")
-public class SpringConfiguration {
-}
